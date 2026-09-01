8c9f1351d345ebef59f7d79f1814fed1caa3dffd
makabaka <1302976601zzy@gmail.com>
2026-09-01 09:45:25 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include "Game.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+int main()
+{
+#ifdef _WIN32
+    // 控制台输入输出统一使用 UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    // 允许 ANSI 颜色转义序列
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE) {
+        DWORD mode = 0;
+        if (GetConsoleMode(hOut, &mode)) {
+            SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        }
+    }
+#endif
+
+    Game game;
+    game.run();
+
+    return 0;
+}
